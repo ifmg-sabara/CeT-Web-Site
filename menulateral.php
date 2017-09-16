cf9e5a0108476996e86542f88b88fd6515003ecd
Deylon Carlo <deyloncarlo@gmail.com>
2017-09-16 05:06:29 -0300

    Trocando ordem de exibição dos itens do menu lateral

--- menulateral.php
+++ menulateral.php
@@ -5,15 +5,15 @@
 					<!--- Menu lateral -->
 					<ul class="menu-lateral">
 						<li class="<?php if ($caminho == ""){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>">Home</a></li>
-						<li class="<?php if ($caminho == "apresentacao-de-trabalhos-cientificos/"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>apresentacao-de-trabalhos-cientificos">Apresentação de Trabalhos Científicos</a></li>
-						<li class="<?php if ($caminho == "ciclo-de-palestras/"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>ciclo-de-palestras">Ciclo de Palestras</a></li>
+						<li class="<?php if ($caminho == "feira-gastronomica/"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>feira-gastronomica">Feira Gastronômica</a></li>
 						<li class="<?php if ($caminho == "feira-de-empreendedorismo/"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>feira-de-empreendedorismo">Feira de Empreendedorismo</a></li>
 						<li class="<?php if ($caminho == "mostra-tecnologica/"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>mostra-tecnologica">Mostra Tecnológica</a></li>
+						<li class="<?php if ($caminho == "apresentacao-de-trabalhos-cientificos/"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>apresentacao-de-trabalhos-cientificos">Apresentação de Trabalhos Científicos</a></li>
+						<li class="<?php if ($caminho == "ciclo-de-palestras/"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>ciclo-de-palestras">Ciclo de Palestras</a></li>
 						<li class="<?php if ($caminho == "oficinas-de-cet/"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>oficinas-de-cet">Oficinas de C & T</a></li>
 						<li class="<?php if ($caminho == "projeto-e-acoes-sociais/"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>projeto-e-acoes-sociais">Projeto e Ações Sociais</a></li>
 						<li class="<?php if ($caminho == "comissao-organizadora/"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>comissao-organizadora">Comissão Organizadora</a></li>
 						<li class="<?php if ($caminho == "cronograma/"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>cronograma">Cronograma</a></li>
-						<li class="<?php if ($caminho == "feira-gastronomica/"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>feira-gastronomica">Feira Gastronômica</a></li>
 						<li class="<?php if ($caminho == "a"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>">Anais</a></li>
 						<li class="<?php if ($caminho == "a"){echo 'active';}else{echo '';}?>"><a href="<?php echo $dominio;?>">Certificados</a></li>
 					</ul>
